0ae5353ea0d046493c6caa01d51493773bbc49f2
Vladimir <shadrov_vladimir@mail.ru>
2021-03-28 22:35:00 +0300

    Выполнена верстка блока Timer

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,6 @@
 import '../styles/styles.scss';
 import '../styles/header.scss';
+import '../styles/timer.scss';
+
+// Перенос изображений
+require.context('../images', true, /\.(png|jpg|svg|gif)$/);
