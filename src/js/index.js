e75b684230649eb5e9129327a3cbaef38cd0789c
Vladimir <shadrov_vladimir@mail.ru>
2021-03-29 15:50:22 +0300

    Добавлена верстка модального окна Settings

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../styles/styles.scss';
 import '../styles/header.scss';
 import '../styles/timer.scss';
+import '../styles/settings.scss';
 
-// Перенос изображений
+// Перенос изображений в папку Dist
 require.context('../images', true, /\.(png|jpg|svg|gif)$/);
